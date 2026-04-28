42c3f726785232849e61a4bff1abf2afc9e9e7a2
Nova Tampati <nova.tampati@gmail.com>
2026-04-28 11:51:38 +0700

    Add files via upload

---
+++ Dictinaries2.py
@@ -0,0 +1,10 @@
+universitas = {
+    "dosen": {
+        "nama": "Dr. Smith",
+        "Jurusan": "Ilmu Komputer"
+        },
+    "mahasiswa": [
+        {"nama": "Ani", "tahun":2},
+        {"nama": "Budi", "tahun":3}
+        ]
+    }
